b4487b44acc7e2f04b128285d924194833dc21c8
kevmo <k.james.moore@gmail.com>
2016-05-22 00:51:32 -0400

    indexHandlerzzzz

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var handlers = require('../lib').handlers;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  return handlers.index(req, res);
 });
 
 module.exports = router;
